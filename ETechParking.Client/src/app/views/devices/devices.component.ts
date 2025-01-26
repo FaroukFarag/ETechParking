31e507fd977d8c6aa258ad9f63ea9cee0ca8547e
Farouk Farag <farouk.farag98@gmail.com>
2025-01-26 14:10:05 +0400

    Migrate to Angular 19.

--- ETechParking.Client/src/app/views/devices/devices.component.ts
+++ ETechParking.Client/src/app/views/devices/devices.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-devices',
-  standalone: true,
-  imports: [],
-  templateUrl: './devices.component.html',
-  styleUrl: './devices.component.css'
+    selector: 'app-devices',
+    imports: [],
+    templateUrl: './devices.component.html',
+    styleUrl: './devices.component.css'
 })
 export class DevicesComponent {
 
