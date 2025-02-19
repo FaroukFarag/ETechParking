f14d37006406caf5aa5ced71566ff9bb90226828
Farouk Farag <farouk.farag98@gmail.com>
2025-02-19 16:47:01 +0100

    Create Lookup Tables.

---
+++ ETechParking.Domain/Lookups/Locations/Fares/FareTypeLookup.cs
@@ -0,0 +1,7 @@
+﻿using ETechParking.Domain.Lookups.Abstraction;
+
+namespace ETechParking.Domain.Lookups.Locations.Fares;
+
+public class FareTypeLookup : BaseLookup
+{
+}
--- ETechParking.Infrastructure.Data/Context/ETechParkingDbContext.cs
+++ ETechParking.Infrastructure.Data/Context/ETechParkingDbContext.cs
@@ -1,4 +1,8 @@
-﻿using ETechParking.Domain.Models.Locations;
+﻿using ETechParking.Domain.Enums.Locations.Fares;
+using ETechParking.Domain.Enums.Locations.Tickets;
+using ETechParking.Domain.Lookups.Locations.Fares;
+using ETechParking.Domain.Lookups.Locations.Tickets;
+using ETechParking.Domain.Models.Locations;
 using ETechParking.Domain.Models.Locations.Fares;
 using ETechParking.Domain.Models.Locations.Tickets;
 using ETechParking.Domain.Models.Locations.Users;
@@ -6,6 +10,7 @@ using ETechParking.Infrastructure.Data.ModelsConfigurations.Locations;
 using ETechParking.Infrastructure.Data.ModelsConfigurations.Locations.Fares;
 using ETechParking.Infrastructure.Data.ModelsConfigurations.Locations.Tickets;
 using ETechParking.Infrastructure.Data.ModelsConfigurations.Locations.Users;
+using ETechParking.Infrastructure.Data.ModelsConfigurations.Lookups.Abstraction;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,6 +21,9 @@ public class ETechParkingDbContext(DbContextOptions options) : IdentityDbContext
     public DbSet<Location> Locations { get; set; }
     public DbSet<Fare> Fares { get; set; }
     public DbSet<Ticket> Tickets { get; set; }
+    public DbSet<FareTypeLookup> FareTypeLookups { get; set; }
+    public DbSet<TransactionTypeLookup> TransactionTypeLookups { get; set; }
+    public DbSet<ClientTypeLookup> ClientTypeLookups { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -25,5 +33,8 @@ public class ETechParkingDbContext(DbContextOptions options) : IdentityDbContext
         modelBuilder.ApplyConfiguration(new FareConfigurations());
         modelBuilder.ApplyConfiguration(new TicketConfigurations());
         modelBuilder.ApplyConfiguration(new UserConfigurations());
+        modelBuilder.ApplyConfiguration(new BaseLookupConfiguration<FareTypeLookup, FareType>("FareTypes"));
+        modelBuilder.ApplyConfiguration(new BaseLookupConfiguration<TransactionTypeLookup, TransactionType>("TransactionTypes"));
+        modelBuilder.ApplyConfiguration(new BaseLookupConfiguration<ClientTypeLookup, ClientType>("ClientTypes"));
     }
 }
