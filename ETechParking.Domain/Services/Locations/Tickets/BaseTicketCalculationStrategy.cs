4b76242a5bce017950df5a4a30ca79f6b267ee38
Farouk Farag <farouk.farag98@gmail.com>
2025-05-14 19:14:14 +0200

    Refactor Tickets Calculation.

---
+++ ETechParking.Domain/Services/Locations/Tickets/BaseTicketCalculationStrategy.cs
@@ -0,0 +1,79 @@
+﻿using ETechParking.Domain.Enums.Locations.Fares;
+using ETechParking.Domain.Interfaces.Services.Locations.Tickets;
+using ETechParking.Domain.Models.Locations;
+using ETechParking.Domain.Models.Locations.Fares;
+using ETechParking.Domain.Models.Locations.Tickets;
+
+namespace ETechParking.Domain.Services.Locations.Tickets;
+
+public abstract class BaseTicketCalculationStrategy : ITicketCalculationStrategy
+{
+    protected abstract FareType VehicleFareType { get; }
+
+    public decimal CalculateTotalFare(Ticket ticket)
+    {
+        Fare fare = GetValidFare(ticket.Location);
+        TimeSpan duration = ticket.ExitDateTime!.Value - ticket.EntryDateTime;
+
+        if (IsWithinFirstHourGrace(duration, fare.EnterGracePeriod))
+            return 0m;
+
+        return CalculateStrictHourlyFare(ticket.EntryDateTime, ticket.ExitDateTime.Value, fare);
+    }
+
+    protected Fare GetValidFare(Location location)
+    {
+        return location.Fares.FirstOrDefault(f => f.FareType == VehicleFareType)
+               ?? throw new InvalidOperationException($"{VehicleFareType} fare not configured");
+    }
+
+    protected virtual bool IsWithinFirstHourGrace(TimeSpan duration, int graceMinutes)
+    {
+        return duration.TotalHours <= 1 && duration.TotalMinutes <= graceMinutes;
+    }
+
+    protected decimal CalculateStrictHourlyFare(DateTime entryTime, DateTime exitTime, Fare fare)
+    {
+        decimal totalFare = 0m;
+        DateTime currentPeriodStart = entryTime;
+
+        while (currentPeriodStart < exitTime)
+        {
+            DateTime periodEnd = Get24HourPeriodEnd(currentPeriodStart, exitTime);
+            TimeSpan periodDuration = periodEnd - currentPeriodStart;
+
+            int billableHours = CalculateExactHours(periodDuration);
+
+            if (fare.MaxLimit.HasValue)
+                billableHours = Math.Min(billableHours, fare.MaxLimit.Value);
+
+            if (billableHours > 0)
+            {
+                totalFare += CalculatePeriodCharge(billableHours, fare);
+            }
+
+            currentPeriodStart = periodEnd;
+        }
+
+        return totalFare;
+    }
+
+    protected virtual decimal CalculatePeriodCharge(int billableHours, Fare fare)
+    {
+        return fare.FirstHourAmount + (billableHours - 1) * fare.Amount;
+    }
+
+    protected DateTime Get24HourPeriodEnd(DateTime start, DateTime exitTime)
+    {
+        DateTime dailyEnd = start.AddHours(24);
+        return exitTime < dailyEnd ? exitTime : dailyEnd;
+    }
+
+    protected virtual int CalculateExactHours(TimeSpan duration)
+    {
+        int fullHours = (int)duration.TotalHours;
+        int remainingMinutes = duration.Minutes;
+
+        return remainingMinutes > 0 ? fullHours + 1 : fullHours;
+    }
+}
--- ETechParking.Domain/Services/Locations/Tickets/BusTicketCalculationStrategy.cs
+++ ETechParking.Domain/Services/Locations/Tickets/BusTicketCalculationStrategy.cs
@@ -1,66 +1,7 @@
 ﻿using ETechParking.Domain.Enums.Locations.Fares;
-using ETechParking.Domain.Interfaces.Services.Locations.Tickets;
-using ETechParking.Domain.Models.Locations.Fares;
-using ETechParking.Domain.Models.Locations.Tickets;
+using ETechParking.Domain.Services.Locations.Tickets;
 
-namespace ETechParking.Domain.Services.Locations.Tickets;
-
-public class BusTicketCalculationStrategy : ITicketCalculationStrategy
+public class BusTicketCalculationStrategy : BaseTicketCalculationStrategy
 {
-    public decimal CalculateTotalFare(Ticket ticket)
-    {
-        Fare fare = ticket.Location.Fares.FirstOrDefault(f => f.FareType == FareType.Bus)!
-            ?? throw new InvalidOperationException("No fare found for the car.");
-        TimeSpan duration = ticket.ExitDateTime!.Value - ticket.EntryDateTime;
-
-        if (duration.TotalMinutes <= fare.EnterGracePeriod)
-        {
-            return 0m;
-        }
-
-        return CalculateFareWithMaxLimit(ticket.EntryDateTime, ticket.ExitDateTime!.Value, fare);
-    }
-
-    private decimal CalculateFareWithMaxLimit(DateTime entryDateTime, DateTime exitDateTime, Fare fare)
-    {
-        decimal totalFare = 0m;
-        DateTime currentPeriodStart = entryDateTime;
-
-        while (currentPeriodStart < exitDateTime)
-        {
-            DateTime currentPeriodEnd = currentPeriodStart.AddHours(24);
-
-            if (exitDateTime < currentPeriodEnd)
-                currentPeriodEnd = exitDateTime;
-
-            TimeSpan periodDuration = currentPeriodEnd - currentPeriodStart;
-
-            int hours = CalculateHoursWithGracePeriod(periodDuration, fare.ExitGracePeriod);
-
-            if (fare.MaxLimit.HasValue && hours > fare.MaxLimit)
-                hours = fare.MaxLimit.Value;
-
-            if (hours > 0)
-            {
-                totalFare += fare.FirstHourAmount + (hours - 1) * fare.Amount;
-            }
-
-            currentPeriodStart = currentPeriodEnd;
-        }
-
-        return totalFare;
-    }
-
-    private int CalculateHoursWithGracePeriod(TimeSpan duration, int exitGracePeriod)
-    {
-        int totalHours = (int)duration.TotalHours;
-        int remainingMinutes = duration.Minutes;
-
-        if (remainingMinutes > exitGracePeriod)
-        {
-            totalHours += 1;
-        }
-
-        return totalHours;
-    }
+    protected override FareType VehicleFareType => FareType.Bus;
 }
